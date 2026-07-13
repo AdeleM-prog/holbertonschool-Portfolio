b2e43b331be96ad22cd55b5ae7f5188299de0575
AdeleM-prog <12043@holbertonstudents.com>
2026-07-13 22:02:01 +0200

    fix: replace wildcard route syntax for Express 5 compatibility

--- alimia/frontend/server.cjs
+++ alimia/frontend/server.cjs
@@ -18,7 +18,7 @@ app.use('/api', createProxyMiddleware({
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
